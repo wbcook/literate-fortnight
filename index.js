c24e3870f0ae45d16088165bef1a7690f30e160f
wbcook <wbcook007@gmail.com>
2016-06-30 15:21:32 -0400

    Add game objects

--- index.js
+++ index.js
@@ -1,25 +1,86 @@
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Working_with_Objects
+// http://stackoverflow.com/questions/5767325/remove-a-particular-element-from-an-array-in-javascript
 var path = require('path');
 var express = require('express');
 var http = require('http').Server();
 var io = require('socket.io')(http);
 var app = express();
 
+// set an environmental variable named port with the app port
 app.set('port', (process.env.PORT || 3000));
 
 // mount the static middleware at the path '/' to serve files from /public
 app.use('/', express.static(path.join(__dirname, 'public')));
 
+// emit chat messages
 io.on('connection', function(socket){
-  console.log('user connected');
+  console.log('user connected at ' + new Date());
+  dealRandomCard();
   socket.on('chat message', function(msg){
     io.emit('chat message', msg);
   });
 });
 
+// cards listen on port 3000
 app.listen(app.get('port'), function() {
   console.log('App started on port: ' + app.get('port'));
 });
 
+// chat listens on port 1337
 http.listen('1337', function() {
   console.log('Http started on port: ' + '1337');
 });
+
+var myDeck = createNewDeck();
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function dealRandomCard() {
+  if (myDeck.cards.length > 0) {
+    var cardIndex = getRandomInt(0, myDeck.cards.length);
+    var randomCard = myDeck.cards.splice(cardIndex, 1)[0];
+    // TODO:
+    // create new player
+    // deal card to player
+    // emit updtated player hand model to the client
+    io.emit('deal hand', randomCard);
+  } else {
+    var err = 'there are no more cards in the deck!';
+    io.emit('deck empty error', err);
+  }
+}
+
+function createNewDeck() {
+  var newDeck = new Deck();
+  for (var i = 0; i < 12; i++) {
+    var name = "card " + i;
+    var rule = "card " + i + " rule"
+    var newCard = new Card(name, rule);
+    newDeck.cards.push(newCard);
+  }
+  return newDeck;
+}
+
+function Card(name, rule) {
+  this.name = name;
+  this.rule = rule;
+}
+
+function Deck() {
+  this.cards = [];
+}
+
+function Player(name) {
+  this.name = name;
+  this.cards = [];
+}
+
+function PlayerList() {
+  this.players = [];
+}
+
+function Table() {
+  this.cards = [];
+}
--- public/js/app.js
+++ public/js/app.js
@@ -1,10 +1,20 @@
 // http://stackoverflow.com/questions/24793255/socket-io-cant-get-it-to-work-having-404s-on-some-kind-of-polling-call
 var socket = io('http://localhost:1337');
-$('#chat-interface').submit(function(){
+$('#chat-button').on('click', function(){
   socket.emit('chat message', $('#chat-input-field').val());
   $('#chat-input-field').val('');
   return false;
 });
 socket.on('chat message', function(msg){
-  $('#chat-room').append($('<li>').text(msg));
+  $('#chat-room').append($('<div>').text(msg));
 });
+socket.on('deal hand', function(card){
+  $('#cards-hand').append('<div class="card">' + card.name + ' | ' + card.rule + '<button class="play-card-button">Play</button></div>');
+});
+socket.on('deck empty error', function(err){
+  $('#cards-hand').append('<div>' + err + '</div>');
+});
+// $('#play-card-button').on('click', function(){
+//   var playedCard = $(this).parent()
+//   socket.emit('play card', /* this card name */);
+// });
